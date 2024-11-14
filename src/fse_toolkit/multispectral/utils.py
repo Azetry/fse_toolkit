cd6303bbdf7b7a3e39797ddd23b358684ce89bda
Hanzel <lin.y.chen109@gmail.com>
2024-11-14 16:10:14 +0800

    FIX(webdriver): Add options let chrome browser start successfully.

--- src/fse_toolkit/multispectral/utils.py
+++ src/fse_toolkit/multispectral/utils.py
@@ -64,6 +64,9 @@ class WebDriverSetup:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')  # 停用GPU加速
+        options.add_argument('--disable-software-rasterizer')  # 停用軟體光柵化
+        options.add_argument('--remote-debugging-port=9222')  # 新增除錯埠
         options.add_experimental_option("prefs", {
             "download.default_directory": self.temp_dir_path,
             "download.prompt_for_download": False,
